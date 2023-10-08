9ed46157e62b96b7801445550524806b948f4d42
Sara Prettyman <saradprettym@gmail.com>
2023-10-07 23:33:38 -0600

    minor debugging

--- src/parse_articles.py
+++ src/parse_articles.py
@@ -2,27 +2,28 @@ import pandas as pd
 from parse_links import get_html
 
 def parse_articles(secrets, links):
-    i = 0
-    articles = []
+    articles = [parse_article(secrets, link) for link in links]
+
+    # Create a dataframe with each article's information
+    return pd.DataFrame(articles, columns=["title", "article", "link", "summary"])
+
+def parse_article(secrets, link):
+    # Get the HTML content of the article
+    html = get_html(secrets, link)
+
+    # Extract the title
+    start_substring = '<!DOCTYPE html><html lang="en"><head><meta charSet="utf-8"/><meta name="viewport" content="width=device-width"/><title>'
+    start_index = html.find(start_substring) + len(start_substring)
+    end_index = html.find('</title>')
+    title = html[start_index:end_index]
+
+    # Extract the article
+    start_substring = ',"articleBody":"'
+    start_index = html.find(start_substring) + len(start_substring)
+    article = html[start_index:html.find("■")]
     
-    # extract article and article title from html to add to list
-    while i < len(links):
-        html = get_html(secrets, links[i])
-        start_substring = '<!DOCTYPE html><html lang="en"><head><meta charSet="utf-8"/><meta name="viewport" content="width=device-width"/><title>'
-        start_index = html.find(start_substring) + len(start_substring)
-        end_index = html.find('</title>')
+    # Replace unnecessary characters
+    article = article.replace("\"", "''").replace("\n", "").replace("\\n", "")
 
-        title = html[start_index:end_index]
-
-        start_substring = ',"articleBody":"'
-        start_index = html.find(start_substring) + len(start_substring)
-
-        article = html[start_index:html.find("■")]
-        article.replace("\"", "''")
-        article.replace("\n", "")
-        article.replace("\\n", "")
-
-        articles.append([title, article, "https://www.economist.com/" + links[i],""])
-        i += 1
-     # create a dataframe with each articles information
-    return pd.DataFrame(articles, columns = ["title", "article", "link", "summary"])
+    # Return the article information
+    return [title, article, "https://www.economist.com" + link, ""]
